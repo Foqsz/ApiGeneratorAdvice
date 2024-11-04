98759f63d457d710ab0c97d4e7850553282ae7cc
Foqsz <contatovictorvinicius05@gmail.com>
2024-11-03 22:58:31 -0300

    refactor / adicioned documentation swagger

--- GetConselho-Api/Program.cs
+++ GetConselho-Api/Program.cs
@@ -1,6 +1,7 @@
 using GetConselho_Api.Services;
 using GetConselho_Api.Services.Interface;
 using GetConselho_Api.Services.Refit;
+using Microsoft.OpenApi.Models;
 using Refit;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+        c.SwaggerDoc("v1", new OpenApiInfo()
+        {
+            Version = "v1",
+            Title = "ConselhosApi",
+            Description = "Api Geradora de Conselhos",
+            TermsOfService = new Uri("https://foqsz.github.io/"),
+            Contact = new OpenApiContact
+            {
+                Name = "Victor Vinicius",
+                Email = "contatovictorvinicius05@gmail.com",
+                Url = new Uri("https://foqsz.github.io/"),
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Usar sobre LICX",
+                Url = new Uri("https://foqsz.github.io/"),
+            }
+        }));
 
 builder.Services.AddScoped<IConselhoService, ConselhoService>();
 builder.Services.AddRefitClient<IConselhoServiceRefit>().ConfigureHttpClient(c =>
